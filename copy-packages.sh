0d4a23aef26afc93b3fb5de6d9d8717524b527d0
Tim Stahlhut <stahta01@gmail.com>
2021-11-28 08:13:56 -0500

    script: Change to crt3 in copy-packages.sh

--- copy-packages.sh
+++ copy-packages.sh
@@ -2,7 +2,7 @@ mkdir -p packages/32bit && mkdir -p packages/64bit && \
 cd opt-mingw-windows-default-manifest && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cp opt-mingw-x86_64-*.pkg.tar.zst ../packages/64bit/ && \
-cd ../opt-mingw-crt2 && \
+cd ../opt-mingw-crt3 && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cp opt-mingw-x86_64-*.pkg.tar.zst ../packages/64bit/ && \
 cd ../opt-mingw-gcc4.6 && \
