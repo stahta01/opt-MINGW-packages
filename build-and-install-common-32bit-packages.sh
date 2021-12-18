5af8a3cf2b057ec46afb99b6d7e24c7b42d9d8e5
Tim Stahlhut <stahta01@gmail.com>
2021-12-18 16:10:31 -0500

    script: Add build-and-install-common-32bit-packages.sh

---
+++ build-and-install-common-32bit-packages.sh
@@ -0,0 +1,37 @@
+_copy_data_to_repo() {
+  for _package in "$@"
+  do
+    local repo="../package_meta_data"
+    if [[ -d  "${repo}" && -d "pkg/${_package}" ]]; then
+        $(cd pkg/${_package} && tree -nif > ../../${repo}/${_package}.txt)
+        $(cd pkg/${_package} && tree -snif > ../../${repo}/${_package}-size.txt)
+        $(
+          cd pkg &&
+          cp ${_package}/.BUILDINFO ../${repo}/${_package}.BUILDINFO
+          cp ${_package}/.PKGINFO ../${repo}/${_package}.PKGINFO
+        )
+    else
+      if [ ! -d  "pkg/${_package}" ]; then
+        echo "Folder pkg/${_package} does not exist"
+      fi
+      if [[ ! -d  "${repo}" ]]; then
+        echo "Folder ${repo} does not exist"
+      fi
+    fi
+  done
+}
+
+cd opt-mingw-windows-default-manifest && \
+../install-build-prerequisites.sh && \
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct -- PKGBUILD) && \
+MINGW_ARCH="mingw32" makepkg-mingw -Lf --install --noconfirm && \
+_copy_data_to_repo opt-mingw-i686-windows-default-manifest && \
+cd ../opt-mingw-bzip2 && \
+MINGW_ARCH="mingw32" makepkg-mingw -Lf --install --noconfirm && \
+_copy_data_to_repo opt-mingw-i686-bzip2 && \
+cd ../opt-mingw-zlib && \
+MINGW_ARCH="mingw32" makepkg-mingw -Lf --install --noconfirm && \
+_copy_data_to_repo opt-mingw-i686-zlib && \
+cd ../opt-mingw-libiconv && \
+MINGW_ARCH="mingw32" makepkg-mingw -Lf --install --noconfirm && \
+_copy_data_to_repo opt-mingw-i686-libiconv opt-mingw-i686-iconv
