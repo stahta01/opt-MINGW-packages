3a3afecb2842e7625f3764af3073c762ba134a02
Tim Stahlhut <stahta01@gmail.com>
2021-12-18 03:19:44 -0500

    script: Add packages to copy-packages.sh

--- copy-packages.sh
+++ copy-packages.sh
@@ -7,4 +7,13 @@ cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cp opt-mingw-x86_64-*.pkg.tar.zst ../packages/64bit/ && \
 cd ../opt-mingw-gcc4.5 && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
-cp opt-mingw-x86_64-*.pkg.tar.zst ../packages/64bit/
+cp opt-mingw-x86_64-*.pkg.tar.zst ../packages/64bit/ && \
+cd ../opt-mingw-bzip2 && \
+cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
+cp opt-mingw-x86_64-*.pkg.tar.zst ../packages/64bit/ && \
+cd ../opt-mingw-zlib && \
+cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
+cp opt-mingw-x86_64-*.pkg.tar.zst ../packages/64bit/ && \
+cd ../opt-mingw-libiconv && \
+cp opt-mingw-i686-*libiconv*.pkg.tar.zst ../packages/32bit/ && \
+cp opt-mingw-x86_64-*libiconv*.pkg.tar.zst ../packages/64bit/
