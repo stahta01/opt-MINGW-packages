662420fe1595a5a696012feeeab336d6d6f529f8
Tim Stahlhut <stahta01@gmail.com>
2021-12-18 03:19:43 -0500

    script: Add build-and-install-gcc4.5-packages.sh

--- copy-packages.sh
+++ copy-packages.sh
@@ -2,9 +2,9 @@ mkdir -p packages/32bit && mkdir -p packages/64bit && \
 cd opt-mingw-windows-default-manifest && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cp opt-mingw-x86_64-*.pkg.tar.zst ../packages/64bit/ && \
-cd ../opt-mingw-crt3 && \
+cd ../opt-mingw-crt2 && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cp opt-mingw-x86_64-*.pkg.tar.zst ../packages/64bit/ && \
-cd ../opt-mingw-gcc4.6 && \
+cd ../opt-mingw-gcc4.5 && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cp opt-mingw-x86_64-*.pkg.tar.zst ../packages/64bit/
