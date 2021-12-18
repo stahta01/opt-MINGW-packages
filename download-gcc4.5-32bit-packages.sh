7417cabc2a43498a0324c9ac7522271449a63126
Tim Stahlhut <stahta01@gmail.com>
2021-12-18 16:10:31 -0500

    script: Add download-gcc4.5-32bit-packages.sh

---
+++ download-gcc4.5-32bit-packages.sh
@@ -0,0 +1,9 @@
+_download_packages() {
+  local git_tag="GCC4.5_alpha_1"
+
+  wget -nc https://github.com/stahta01/opt-MINGW-packages/releases/download/${git_tag}/32bit.zip && \
+  7za e 32bit.zip && \
+  rm -fr 32bit.zip
+}
+
+mkdir -p packages && cd packages && _download_packages
