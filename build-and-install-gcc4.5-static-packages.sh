93940dd9c10ee2a1ef23c7b9f1b3905f484c455d
Tim Stahlhut <stahta01@gmail.com>
2021-12-18 03:19:44 -0500

    script: Add build-and-install-gcc4.5-static-packages.sh

---
+++ build-and-install-gcc4.5-static-packages.sh
@@ -0,0 +1,29 @@
+_copy_data_to_repo() {
+  for _package in "$@"
+  do
+    local repo="../package_meta_data"
+    if [[ -d  "${repo}" && -d "pkg/${_package}" ]]; then
+        $(cd pkg/${_package} && tree -nif > ../../${repo}/${_package}.txt)
+        $(cd pkg/${_package} && tree -snif > ../../${repo}/${_package}-size.txt)
+        $(
+          cd pkg &&
+          cp ${_package}/.BUILDINFO ../${repo}/${_package}.BUILDINFO
+          cp ${_package}/.PKGINFO ../${repo}/${_package}.PKGINFO
+        )
+    else
+      if [ ! -d  "pkg/${_package}" ]; then
+        echo "Folder pkg/${_package} does not exist"
+      fi
+      if [[ ! -d  "${repo}" ]]; then
+        echo "Folder ${repo} does not exist"
+      fi
+    fi
+  done
+}
+
+cd opt-mingw-gcc4.5-static && \
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct -- PKGBUILD) && \
+MINGW_ARCH="mingw32" makepkg-mingw -Lf --install --noconfirm && \
+_copy_data_to_repo opt-mingw-i686-gcc4.5-static && \
+MINGW_ARCH="mingw64" makepkg-mingw -Lf --install --noconfirm && \
+_copy_data_to_repo opt-mingw-x86_64-gcc4.5-static
