cafa8dcc2471f7b1906b6c5f99a1b816a9201669
Tim Stahlhut <stahta01@gmail.com>
2021-12-18 16:10:31 -0500

    script: Add download-gcc4.5-packages.sh

---
+++ download-gcc4.5-packages.sh
@@ -0,0 +1,12 @@
+_download_packages() {
+  local git_tag="GCC4.5_alpha_1"
+
+  wget -nc https://github.com/stahta01/opt-MINGW-packages/releases/download/${git_tag}/32bit.zip && \
+  wget -nc https://github.com/stahta01/opt-MINGW-packages/releases/download/${git_tag}/64bit.zip && \
+  7za e 32bit.zip && \
+  7za e 64bit.zip && \
+  rm -fr 32bit.zip && \
+  rm -fr 64bit.zip
+}
+
+mkdir -p packages && cd packages && _download_packages
