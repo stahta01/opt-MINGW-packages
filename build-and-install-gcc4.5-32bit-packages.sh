9723467bd02ed23fb8de26f8ffa4515282d4375b
Tim Stahlhut <stahta01@gmail.com>
2021-12-18 16:10:31 -0500

    script: Add build-and-install-gcc4.5-32bit-packages.sh

---
+++ build-and-install-gcc4.5-32bit-packages.sh
@@ -0,0 +1,28 @@
+_copy_data_to_repo() {
+  for _package in "$@"
+  do
+    local repo="../package_meta_data"
+    if [[ -d  "${repo}" && -d "pkg/${_package}" ]]; then
+        $(cd pkg/${_package} && tree -nif > ../../${repo}/${_package}.txt)
+        $(cd pkg/${_package} && tree -snif > ../../${repo}/${_package}-size.txt)
+        $(
+          cd pkg &&
+          cp ${_package}/.BUILDINFO ../${repo}/${_package}.BUILDINFO
+          cp ${_package}/.PKGINFO ../${repo}/${_package}.PKGINFO
+        )
+    else
+      if [ ! -d  "pkg/${_package}" ]; then
+        echo "Folder pkg/${_package} does not exist"
+      fi
+      if [[ ! -d  "${repo}" ]]; then
+        echo "Folder ${repo} does not exist"
+      fi
+    fi
+  done
+}
+
+cd opt-mingw-gcc4.5 && \
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct -- PKGBUILD) && \
+MINGW_ARCH="mingw32" makepkg-mingw -Lf --install --noconfirm && \
+_copy_data_to_repo opt-mingw-i686-gcc4.5 opt-mingw-i686-gcc4.5-libs \
+  opt-mingw-i686-gcc4.5-fortran opt-mingw-i686-gcc4.5-libgfortran opt-mingw-i686-gcc4.5-objc
