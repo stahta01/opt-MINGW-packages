1677d732e0a47cb8ca5862efae8a30712af6fd60
Tim Stahlhut <stahta01@gmail.com>
2021-12-19 20:28:18 -0500

    script: Remove bzip2 and libiconv from build-and-install-common-32bit-packages.sh

--- build-and-install-common-32bit-packages.sh
+++ build-and-install-common-32bit-packages.sh
@@ -26,12 +26,6 @@ cd opt-mingw-windows-default-manifest && \
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct -- PKGBUILD) && \
 MINGW_ARCH="mingw32" makepkg-mingw -Lf --install --noconfirm && \
 _copy_data_to_repo opt-mingw-i686-windows-default-manifest && \
-cd ../opt-mingw-bzip2 && \
-MINGW_ARCH="mingw32" makepkg-mingw -Lf --install --noconfirm && \
-_copy_data_to_repo opt-mingw-i686-bzip2 && \
 cd ../opt-mingw-zlib && \
 MINGW_ARCH="mingw32" makepkg-mingw -Lf -C --install --noconfirm && \
-_copy_data_to_repo opt-mingw-i686-zlib && \
-cd ../opt-mingw-libiconv && \
-MINGW_ARCH="mingw32" makepkg-mingw -Lf --install --noconfirm && \
-_copy_data_to_repo opt-mingw-i686-libiconv opt-mingw-i686-iconv
+_copy_data_to_repo opt-mingw-i686-zlib
