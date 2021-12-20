52b32695095ef32a059a36ed660fed0898e2b4ce
Tim Stahlhut <stahta01@gmail.com>
2021-12-19 20:28:18 -0500

    script: Remove bzip2 and libiconv from copy-32bit-packages.sh

--- copy-32bit-packages.sh
+++ copy-32bit-packages.sh
@@ -5,11 +5,7 @@ cd ../opt-mingw-crt2 && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cd ../opt-mingw-gcc4.5 && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
-cd ../opt-mingw-bzip2 && \
-cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cd ../opt-mingw-zlib && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cd ../opt-mingw-binutils2.23 && \
-cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
-cd ../opt-mingw-libiconv && \
-cp opt-mingw-i686-*libiconv*.pkg.tar.zst ../packages/32bit/
+cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/
