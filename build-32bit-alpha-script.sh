dc28beaafecf77264630ad8cc319ce5c16ded506
Tim Stahlhut <stahta01@gmail.com>
2021-12-21 15:09:46 -0500

    script: Move install prerequisites higher in build-32bit-alpha-script.sh

--- build-32bit-alpha-script.sh
+++ build-32bit-alpha-script.sh
@@ -6,13 +6,13 @@ _build_alpha_32bit() {
   pacman -R --noconfirm --cascade opt-mingw-i686-zlib           || true && \
   pacman -R --noconfirm --cascade opt-mingw-i686-headers2-git   || true && \
   pacman -R --noconfirm --cascade opt-mingw-i686-headers3-git   || true && \
+  ./install-build-prerequisites.sh                                      && \
   ./download-base_libs-packages.sh                                      && \
   cd packages/base_libs && pacman -U opt-mingw-i686-*.zst --noconfirm   && \
   cd ../..                                                              && \
   cd packages && rm -f *.zst && rm -f *.zip && cd ..                    && \
   ./download-gcc4.5-32bit-packages.sh                                   && \
   cd packages && pacman -U *.zst --noconfirm && cd ..                   && \
-  ./install-build-prerequisites.sh                                      && \
   ./build-and-install-gcc4.5-32bit-packages.sh                          && \
   ./build-and-install-crt2-32bit-packages.sh                            && \
   ./build-and-install-binutils2.23-32bit-packages.sh                    && \
