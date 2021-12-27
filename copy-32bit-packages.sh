05e6baab51882a7f19321679c79ac5bf383c2224
Tim Stahlhut <stahta01@gmail.com>
2021-12-27 05:45:14 -0500

    script: Copy gcc4 in copy-32bit-packages.sh

--- copy-32bit-packages.sh
+++ copy-32bit-packages.sh
@@ -3,7 +3,7 @@ cd opt-mingw-windows-default-manifest && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cd ../opt-mingw-crt1 && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
-cd ../opt-mingw-gcc4.5 && \
+cd ../opt-mingw-gcc4 && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
 cd ../opt-mingw-zlib && \
 cp opt-mingw-i686-*.pkg.tar.zst ../packages/32bit/ && \
