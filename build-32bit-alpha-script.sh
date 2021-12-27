3883b1d5bcbb24f2573583b36076e6b92892cada
Tim Stahlhut <stahta01@gmail.com>
2021-12-27 05:45:14 -0500

    script: Stop download and build gcc4 git in build-32bit-alpha-script.sh

--- build-32bit-alpha-script.sh
+++ build-32bit-alpha-script.sh
@@ -11,16 +11,19 @@ _build_alpha_32bit() {
   ./download-base_libs-packages.sh                                      && \
   cd packages/base_libs && pacman -U opt-mingw-i686-*.zst --noconfirm   && \
   cd ../..                                                              && \
-  cd packages && rm -f *.zst && rm -f *.zip && cd ..                    && \
-  ./download-gcc4.5-32bit-packages.sh                                   && \
   cd packages && pacman -U *.zst --noconfirm && cd ..                   && \
   pacman -R --noconfirm --cascade opt-mingw-i686-gcc4.5-libgfortran || true && \
   pacman -R --noconfirm --cascade opt-mingw-i686-gcc4.5-objc    || true && \
   pacman -S --needed --noconfirm mingw-w64-i686-binutils                && \
+  ./build-and-install-gcc4-32bit-packages.sh                            && \
   ./build-and-install-crt1-32bit-packages.sh                            && \
   ./build-and-install-binutils2.23-32bit-packages.sh                    && \
-  ./build-and-install-gcc4.5-32bit-packages.sh                          && \
+  ./build-and-install-gcc4-32bit-packages.sh                            && \
   ./build-and-install-common-32bit-packages.sh
 }
 
 _build_alpha_32bit
+
+#  cd packages && rm -f *.zst && rm -f *.zip && cd ..                    && \
+#  ./download-gcc4.5-32bit-packages.sh                                   && \
+#
